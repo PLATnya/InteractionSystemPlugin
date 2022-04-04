c401320d36ebd78072ff79f3f217ca55e41911a6
PLATnya <vadim.plataniy@gmail.com>
2022-04-04 20:34:38 +0300

    Add interaction interface for characters setup (doesn't work)

---
+++ Source/InteractionSystem/ISCharacterInteractionInterface.h
@@ -0,0 +1,61 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ISCharacterInteractionInterface.generated.h"
+
+class AMInteractable;
+class UPhysicsConstraintComponent;
+class UPhysicsHandleComponent;
+// This class does not need to be modified.
+UINTERFACE()
+class UISCharacterInteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class INTERACTIONSYSTEM_API IISCharacterInteractionInterface
+{
+	GENERATED_BODY()
+
+	TArray<AMInteractable*> ActiveItems;
+	
+public:
+	void BeginInteraction();
+	
+	void UpdateInteraction(float _DeltaTime);
+	
+	//TODO: refresh
+	UFUNCTION(BlueprintNativeEvent)
+	FVector GetGrabbingLocation() const;
+	
+	//TODO: refresh
+	UFUNCTION(BlueprintNativeEvent)
+	FRotator GetGrabbingRotation() const;
+	
+	UFUNCTION(BlueprintCallable)
+	void AddActiveItem(class AMInteractable* pItem);
+	//TODO: refresh
+	UFUNCTION(BlueprintCallable)
+	void RemoveActiveItem(class AMInteractable* pItem);
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	const TArray<AMInteractable*>& GetActiveItems() const { return ActiveItems; }
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	const TArray<AMInteractable*> GetActiveItemsCopy() const { return ActiveItems; }
+
+	
+	virtual UPhysicsConstraintComponent* GetPhysicsConstraintComponent() const = 0;
+	virtual UPhysicsHandleComponent* GetPhysicsHandleComponent() const = 0;
+
+	virtual void OnPhysicsConstraintBroken(int32 _ConstraintIndex);
+private:
+	void UpdateHandle(float _DeltaTime);
+	void UpdateConstraints(float _DeltaTime);
+};
